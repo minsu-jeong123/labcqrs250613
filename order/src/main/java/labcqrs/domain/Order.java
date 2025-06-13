4c0cc0a8c12112dce41ac6d47ceb581104b2444e
minsu jeong <alstndldl@naver.com>
2025-06-13 07:16:39 +0000

    MyPage, Delivery, Order 로직 수정

--- order/src/main/java/labcqrs/domain/Order.java
+++ order/src/main/java/labcqrs/domain/Order.java
@@ -38,7 +38,9 @@ public class Order {
     public void onPostPersist() {
         OrderPlaced orderPlaced = new OrderPlaced(this);
         orderPlaced.publishAfterCommit();
-
+    }
+    @PreRemove
+    public void onPreRemove() {
         OrderCancelled orderCancelled = new OrderCancelled(this);
         orderCancelled.publishAfterCommit();
     }
